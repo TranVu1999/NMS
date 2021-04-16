dbaaee549225d1c8d9d1a7bb07b8b5d9ce8b330b
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2021-04-16 14:57:16 +0700

    comlete form login and sign-in

---
+++ NMS/app/src/main/java/tranvu203107/dmt/nms/RegisterActivity.java
@@ -0,0 +1,36 @@
+package tranvu203107.dmt.nms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        changeStatusBarColor();
+    }
+
+    public void changeStatusBarColor(){
+        // for changing  status bar icon color
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.register_bk_color));
+
+        }
+    }
+
+    public void onLoginClick(View v){
+        startActivity(new Intent(this, LoginActivity.class));
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_in_right);
+    }
+}
