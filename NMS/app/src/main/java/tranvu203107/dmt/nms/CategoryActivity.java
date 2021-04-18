51eb153911f84faf3dae5d189584a7f130ea0292
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2021-04-18 22:38:30 +0700

    complete task design category page

--- NMS/app/src/main/java/tranvu203107/dmt/nms/CategoryActivity.java
+++ NMS/app/src/main/java/tranvu203107/dmt/nms/CategoryActivity.java
@@ -1,14 +1,71 @@
 package tranvu203107.dmt.nms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class CategoryActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    ListView listView;
+    ListView listViewAccount;
+
+    ArrayList<ItemMenu> arrList;
+    ArrayList<ItemMenu> arrListAccount;
+    MenuAdapter menuAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        // map
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        listView = (ListView) findViewById(R.id.listView);
+        listViewAccount = (ListView) findViewById(R.id.listViewAccount);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        // Config toolbar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_action_menu);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                drawerLayout.openDrawer(GravityCompat.START );
+            }
+        });
+
+        // action menu
+        arrList = new ArrayList<ItemMenu>();
+        arrList.add(new ItemMenu("Home", R.drawable.ic_action_home));
+        arrList.add(new ItemMenu("Category", R.drawable.ic_action_category));
+        arrList.add(new ItemMenu("Priority", R.drawable.ic_action_priority));
+        arrList.add(new ItemMenu("Status", R.drawable.ic_action_status));
+        arrList.add(new ItemMenu("Note", R.drawable.ic_action_note));
+
+        menuAdapter = new MenuAdapter(this, R.layout.item_row_menu, arrList);
+        listView.setAdapter(menuAdapter);
+
+        // action menu account
+        arrListAccount = new ArrayList<ItemMenu>();
+        arrListAccount.add(new ItemMenu("Edit Profile", R.drawable.ic_action_edit));
+        arrListAccount.add(new ItemMenu("Change Password", R.drawable.ic_action_change));
+
+        menuAdapter = new MenuAdapter(this, R.layout.item_row_menu, arrListAccount);
+        listViewAccount.setAdapter(menuAdapter);
     }
 }
