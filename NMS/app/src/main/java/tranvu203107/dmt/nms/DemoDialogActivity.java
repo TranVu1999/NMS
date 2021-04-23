d3a7f7b9899da001ccab2ca2f95eda60ec5a6742
Thinhnnn <thinhtm99@gmail.com>
2021-04-23 15:17:24 +0700

    select plan date

--- NMS/app/src/main/java/tranvu203107/dmt/nms/DemoDialogActivity.java
+++ NMS/app/src/main/java/tranvu203107/dmt/nms/DemoDialogActivity.java
@@ -7,8 +7,10 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -20,7 +22,7 @@ public class DemoDialogActivity extends AppCompatActivity {
     Button btnAddCate, btnAddNote;
     Dialog dialog;
 
-    Spinner spinnerCate;
+    Spinner spinnerCate, spinnerPriority, spinnerStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,28 +41,12 @@ public class DemoDialogActivity extends AppCompatActivity {
                 openAddCategoryDialog();
             }
         });
-//        btnAddNote.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openAddNoteDialog();
-//            }
-//        });
-//
-//
-//
-//        // Create list option
-//        ArrayList<String> arr = new ArrayList<String>();
-//        arr.add("Tran Le Anh Vu");
-//        arr.add("Pham Duc Huy");
-//        arr.add("Nguyen Hoang Toan");
-//        arr.add("Tran Minh Thinh");
-//        arr.add("Huynh Ngoc Quoc");
-//
-//        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, arr);
-//        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item  );
-//        spinnerCate.setAdapter(arrayAdapter);
-
-
+        btnAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddNoteDialog();
+            }
+        });
 
     }
 
@@ -85,8 +71,88 @@ public class DemoDialogActivity extends AppCompatActivity {
 
         Button btnCloseDialog = dialog.findViewById(R.id.btnCloseDialog);
         Button btnSaveData = dialog.findViewById(R.id.btnSaveData);
+        Button btnSelectPlanDate = dialog.findViewById(R.id.btnSelectPlanDate);
+        btnSelectPlanDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSelectDateDialog();
+            }
+        });
+
+        OptionAdapter optionsAdapter;
+        spinnerCate = dialog.findViewById(R.id.spinnerCate);
+        spinnerPriority = dialog.findViewById(R.id.spinnerPrioroty);
+        spinnerStatus = dialog.findViewById(R.id.spinnerStatus);
+        ArrayList<Option> arrOptionCate;
+        // action menu
+        arrOptionCate = new ArrayList<Option>();
+        arrOptionCate.add(new Option("Cate 1", "1"));
+        arrOptionCate.add(new Option("Cate 2", "2"));
+        arrOptionCate.add(new Option("Cate 3", "3"));
+        arrOptionCate.add(new Option("Cate 4", "4"));
+
+        ArrayList<Option> arrOptionPriority;
+        // action menu
+        arrOptionPriority = new ArrayList<Option>();
+        arrOptionPriority.add(new Option("Prio 1", "1"));
+        arrOptionPriority.add(new Option("Prio 2", "2"));
+        arrOptionPriority.add(new Option("Prio 3", "3"));
+
+        ArrayList<Option> arrOptionStatus;
+        // action menu
+        arrOptionStatus = new ArrayList<Option>();
+        arrOptionStatus.add(new Option("Status 1", "1"));
+        arrOptionStatus.add(new Option("Status 2", "2"));
+        arrOptionStatus.add(new Option("Status 3", "3"));
+
+        optionsAdapter = new OptionAdapter(this, R.layout.layout_option_item, arrOptionCate);
+        spinnerCate.setAdapter(optionsAdapter);
+
+        optionsAdapter = new OptionAdapter(this, R.layout.layout_option_item, arrOptionPriority);
+        spinnerPriority.setAdapter(optionsAdapter);
+
+        optionsAdapter = new OptionAdapter(this, R.layout.layout_option_item, arrOptionStatus);
+        spinnerStatus.setAdapter(optionsAdapter);
+
+        spinnerStatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                Toast.makeText(getApplicationContext(), "This is a message" + position, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
 
         dialog.show();
     }
 
+    private void openSelectDateDialog(){
+        //mở layout chọn ngày lên
+        dialog.setContentView(R.layout.layout_select_plandate);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+
+        CalendarView calendarView;
+        calendarView = dialog.findViewById(R.id.cldPlanDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                String  curDate = String.valueOf(dayOfMonth);
+                String  Year = String.valueOf(year);
+                String  Month = String.valueOf(month);
+
+                Toast toast = Toast.makeText(getApplicationContext(), Year+"/"+Month+"/"+curDate, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        dialog.show();
+    }
 }
